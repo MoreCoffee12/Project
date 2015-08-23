f7b569e3e0aa87dedd081962e0edd2c9210b8773
MoreCoffee12 <brian.howard@ihmail.com>
2015-08-23 12:58:01 -0700

    Corrected read.csv formats

--- run_analysis.R
+++ run_analysis.R
@@ -4,17 +4,26 @@ setwd("C:/Users/Brian/OneDrive/DataScience/GettingAndCleaningData/Project")
 ## You should create one R script called run_analysis.R that does the following. 
 
 ## 1. Merges the training and the test sets to create one data set.
-strSubjectTestPath = "./UCI HAR Dataset/test/subject_test.txt"
-SubjectTest = read.csv(strSubjectTestPath)
 
+# Begin with the dataset descriptor files
+strFeatures <- "./UCI HAR Dataset/features.txt"
+Features <- read.csv(strFeatures, header = FALSE, sep ="")
+
+strSubjectTestPath = "./UCI HAR Dataset/test/subject_test.txt"
+SubjectTest = read.csv(strSubjectTestPath, header = FALSE, sep="")
+
+# Load the test data set
 strXTestPath = "./UCI HAR Dataset/test/X_test.txt"
 XTest = read.csv(strXTestPath, header = FALSE, sep="")
 
+# Add the names from the features.txt file
+names(XTest) <- Features[,2]
+
 strYTestPath = "./UCI HAR Dataset/test/y_test.txt"
-YTest = read.csv(strYTestPath, header = FALSE, sep=" ")
+YTest = read.csv(strYTestPath, header = FALSE, sep="")
 
 strSubjectTrainPath = "./UCI HAR Dataset/train/subject_train.txt"
-SubjectTrain = read.csv(strSubjectTrainPath)
+SubjectTrain = read.csv(strSubjectTrainPath, header = FALSE, sep="")
 
 
 ## 2. Extracts only the measurements on the mean and standard deviation for each 
